d7355c08e4b4a1c174f59b6e044614724b410ca6
mattchristenson <matt@rcc.team>
2022-10-18 08:12:00 -0500

    simple math problems

---
+++ hello.js
@@ -0,0 +1,28 @@
+console.log("Hello Class");
+
+
+var firstName = "Matt";
+var lastName = "Christenson";
+
+var fullName = lastName + ", " + firstName;
+console.log(fullName);
+
+console.log(lastName + ", " + firstName);
+
+var num1 = 56;
+var num2 = 20;
+var answer1 = num1 + num2;
+var answer2 = num1 - num2;
+var answer3 = num1 * num2;
+var answer4 = num1 / num2;
+var answer5 = num1 % num2;
+
+console.log("1: " + answer1);
+
+console.log("2: " + answer2);
+
+console.log("3: " + answer3);
+
+console.log("4: " + answer4);
+
+console.log("5: " + answer5);
